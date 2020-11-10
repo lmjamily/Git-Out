0492aced52e765304cb9322995534f49553c9e91
Vitória Brunielly <vitoriabrunielly25@gmail.com>
2020-11-10 11:22:05 -0300

    Arquivo Impar

---
+++ iep.py
@@ -0,0 +1,14 @@
+P = int(input())
+d1 = int(input())
+d2= int(input())
+c= (d1+d2)
+if P == 0:
+    if c%2==0:
+        print("0")
+    else:
+        print("1")
+if P==1:
+    if c%2==1:
+        print("0")
+    else:
+        print("1")
